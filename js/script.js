e2aa5c8497dc95ddb9987df2c44f087e8001ae71
FarhatA22 <farhatabd21@gmail.com>
2020-12-14 16:20:50 +0700

    upload tugas besar

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const textelement = ['Mahasiswa Teknik Informatika','Programmer','Gamers'];
+let count = 0;
+let txtIndex = 0;
+let currentTxt ='';
+let words ='';
+
+(function ngetik(){
+
+    if(count == textelement.length) {
+        count = 0;
+    }
+
+    currentTxt = textelement[count];
+
+    words = currentTxt.slice(0, ++txtIndex);
+    document.querySelector('.efek-ngetik').textContent = words;
+
+    if(words.length ==currentTxt.length) {
+        count++;
+        txtIndex = 0;
+    }
+
+    setTimeout(ngetik, 500);
+
+})()
